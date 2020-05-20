d2518dc3a5da9c5dbda253e542a46d0bbb1c850a
midhunkm1294 <46639881+midhunkm1294@users.noreply.github.com>
2020-05-20 16:02:42 +0530

    Update deploy.py

--- userbot/plugins/deploy.py
+++ userbot/plugins/deploy.py
@@ -13,7 +13,7 @@ from uniborg.util import admin_cmd
 from userbot import AUTONAME
 
 
-DEFAULTUSER = str(AUTONAME) if AUTONAME else "GujjuBot"
+DEFAULTUSER = str(AUTONAME) if AUTONAME else "🇮🇳"
 
 @borg.on(admin_cmd(pattern=r"deploy"))
 
@@ -35,7 +35,7 @@ async def _(event):
 
     animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (GujjuBot)**",
+            "**Heroku Connecting To Latest Github Build (🇮🇳)**",
             "**Build started by user** **{DEFAULTUSER}**",
             "**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
             "**Restarting Heroku Server...**",
